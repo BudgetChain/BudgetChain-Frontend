22528f0b130cea5598a108d7592f0a0f1ee47230
frankhood <youremail@domain.com>
2025-03-30 11:49:30 +0100

    chore: ensure consistency with figma design

--- src/app/dashboard/organization/page.tsx
+++ src/app/dashboard/organization/page.tsx
@@ -28,7 +28,7 @@ function OrganizationDashboard() {
           />
         </div>
 
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 mb-6 w-full">
+        <div className="grid grid-cols-1 md:grid-cols-[2fr_1fr] gap-4 mb-6 w-full">
           <AIAnalysis />
           <div className="grid grid-cols-1 gap-4 w-full">
             <PendingAppeals />
